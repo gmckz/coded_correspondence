0d6a86cadc99ac536c2adcc82dbdf06f940a04e6
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-11-02 17:59:43 +0000

    caesar cipher encoder test driven

--- lib/caesar_cipher.py
+++ lib/caesar_cipher.py
@@ -11,3 +11,17 @@ def caesar_decode(message, offset):
             decoded_message += char
     print(decoded_message)
     return decoded_message
+
+def caesar_encode(message, offset):
+    encoded_message = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for char in message:
+        if char in alphabet:
+            starting_index = alphabet.find(char)
+            ending_index = starting_index - offset
+            new_char = alphabet[ending_index % 26]
+            encoded_message += new_char
+        else:
+            encoded_message += char
+    print(encoded_message)
+    return encoded_message
