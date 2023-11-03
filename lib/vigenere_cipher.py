6b23d4131efd83221ce03e4acf7742689d7a615c
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-11-03 11:29:14 +0000

    vigenere_encode returns encoded message given message and keyword

--- lib/vigenere_cipher.py
+++ lib/vigenere_cipher.py
@@ -25,3 +25,31 @@ def vigenere_decode(message, keyword):
             decoded_message += message[i]
     print(decoded_message)
     return decoded_message
+
+def vigenere_encode(message, keyword):
+    encoded_message = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    keyword_phrase = ""
+    keyword_index = 0
+    for char in message:
+        if char in alphabet:
+            keyword_phrase += keyword[keyword_index]
+            if keyword_index < len(keyword) - 1:
+                keyword_index += 1
+            else:
+                keyword_index = 0
+        else:
+            keyword_phrase += char
+
+    for i in range(len(message)):
+        if message[i] in alphabet:
+            offset = alphabet.find(keyword_phrase[i])
+            starting_index = alphabet.find(message[i])
+            ending_index = starting_index - offset
+            new_char = alphabet[ending_index % 26]
+            encoded_message += new_char
+        else:
+            encoded_message += message[i]
+    print(encoded_message)
+    return encoded_message
