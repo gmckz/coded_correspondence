b6b61da4502dea35852f567a686723fd594eea40
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-11-03 11:27:43 +0000

    vigenere_decode returns decoded message given message and keyword

---
+++ lib/vigenere_cipher.py
@@ -0,0 +1,27 @@
+def vigenere_decode(message, keyword):
+    decoded_message = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    keyword_phrase = ""
+    keyword_index = 0
+    for char in message:
+        if char in alphabet:
+            keyword_phrase += keyword[keyword_index]
+            if keyword_index < len(keyword) - 1:
+                keyword_index += 1
+            else:
+                keyword_index = 0
+        else:
+            keyword_phrase += char
+
+    for i in range(len(message)):
+        if message[i] in alphabet:
+            offset = alphabet.find(keyword_phrase[i])
+            starting_index = alphabet.find(message[i])
+            ending_index = starting_index + offset
+            new_char = alphabet[ending_index % 26]
+            decoded_message += new_char
+        else:
+            decoded_message += message[i]
+    print(decoded_message)
+    return decoded_message
--- tests/test_vigenere_cipher.py
+++ tests/test_vigenere_cipher.py
@@ -1,1 +1,13 @@
 from lib.vigenere_cipher import *
+
+def test_vigenere_decode():
+    message = "txm srom vkda gl lzlgzr qpdb? fepb ejac! ubr imn tapludwy mhfbz cza ruxzal wg zztcgcexxch!"
+    keyword = "friends"
+    decoded_message = "you were able to decode this? nice work! you are becoming quite the expert at crytography!"
+    assert vigenere_decode(message, keyword) == decoded_message
+
+def test_vigenere_encode():
+    message = "you were able to decode this? nice work! you are becoming quite the expert at crytography!"
+    keyword = "friends"
+    encoded_message = "txm srom vkda gl lzlgzr qpdb? fepb ejac! ubr imn tapludwy mhfbz cza ruxzal wg zztcgcexxch!"
+    assert vigenere_encode(message, keyword) == encoded_message
